30835254244407ecd064b48dbf75f4ea019338d6
Marius <jaunikis@gmail.com>
2017-12-03 22:42:14 +0000

    mktrans fb

--- mktransport/footer.php
+++ mktransport/footer.php
@@ -1,6 +1,9 @@
 <section class="bg-dark text-white" style="padding:10px;padding-top:50px">
       <div class="container text-center">
-        <div class="fb-share-button" data-href="http://www.mk-transport.lt" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.mk-transport.lt%2F&amp;src=sdkpreparse">Bendrinti</a></div>
+        
+		<div class="fb-like" data-href="http://www.mk-transport.lt" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+	
+		<!--<div class="fb-share-button" data-href="http://www.mk-transport.lt" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.mk-transport.lt%2F&amp;src=sdkpreparse">Dalintis</a></div>-->
 		<p style="padding-top:50px;"><span style="font-size:10px;">All Copyrights Reserved by mk-transport.lt @ 2017 - Made by <a href="#">mvmedia</a></span></p>
         
       </div>
